e3ce41fa0bbb92388774d924a494cd5b6e71116e
Muirghen <vasquezmariano01@gmail.com>
2023-01-19 19:49:21 -0400

    Added users connection to Accounts

--- MonkeyBusiness/Models/User.cs
+++ MonkeyBusiness/Models/User.cs
@@ -13,5 +13,6 @@ namespace MonkeyBusiness.Models
         public string Name { get; set; }
         public string Lastname { get; set; }
         public string Password { get; set; }
+        public List<Account> Account { get; set; }
     }
 }
